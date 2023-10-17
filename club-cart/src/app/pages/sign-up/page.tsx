1b7163a5cdf087b3f17174686755565c5acd1f81
Praneeth Otthi <73143434+PraneethO@users.noreply.github.com>
2023-10-16 20:36:20 -0400

    started work on page

--- club-cart/src/app/pages/sign-up/page.tsx
+++ club-cart/src/app/pages/sign-up/page.tsx
@@ -2,6 +2,117 @@
 
 import styles from "./page.module.css";
 
+import { useState } from "react";
+
 export default function SignUp() {
-  return <div>Sign Up Page</div>;
+  const [studentForm, setStudentForm] = useState(true);
+
+  const [school, setSchool] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.firstContainer}>
+        <div className={styles.leftPartition}>CLUBCART</div>
+        <div className={styles.rightPartition}>
+          <div className={styles.buttonSwitch}>
+            <button
+              className={styles.switchButton}
+              onClick={() => {
+                setStudentForm(true);
+              }}
+              style={
+                studentForm
+                  ? { backgroundColor: "rgb(4, 78, 139)" }
+                  : { background: "transparent" }
+              }
+            >
+              Student
+            </button>
+            <button
+              className={styles.switchButton}
+              onClick={() => {
+                setStudentForm(false);
+              }}
+              style={
+                !studentForm
+                  ? { backgroundColor: "rgb(4, 78, 139)" }
+                  : { background: "transparent" }
+              }
+            >
+              Authorization
+            </button>
+          </div>
+          {studentForm ? (
+            <div className={styles.loginForm}>
+              <div
+                className={styles.inputContainer}
+                style={{ marginTop: "1rem" }}
+              >
+                <div className={styles.inputDesc}>School</div>
+                <input
+                  className={styles.inputField}
+                  placeholder="Enter your school"
+                  value={school}
+                  onChange={(e) => {
+                    setSchool(e.target.value);
+                  }}
+                  style={{ marginBottom: "0.5rem" }}
+                ></input>
+              </div>
+              <div
+                className={styles.inputContainer}
+                style={{ marginTop: "1rem" }}
+              >
+                <div className={styles.inputDesc}>Email</div>
+                <input
+                  className={styles.inputField}
+                  placeholder="Enter your email"
+                  value={email}
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                  style={{ marginBottom: "0.5rem" }}
+                ></input>
+              </div>
+              <div
+                className={styles.inputContainer}
+                style={{ marginTop: "1rem" }}
+              >
+                <div className={styles.inputDesc}>Password</div>
+                <input
+                  className={styles.inputField}
+                  placeholder="Enter your password"
+                  value={password}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                  style={{ marginBottom: "0.5rem" }}
+                ></input>
+              </div>
+              <div
+                className={styles.inputContainer}
+                style={{ marginTop: "1rem" }}
+              >
+                <div className={styles.inputDesc}>Confirm Password</div>
+                <input
+                  className={styles.inputField}
+                  placeholder="Confirm Your Password"
+                  value={confirmPassword}
+                  onChange={(e) => {
+                    setConfirmPassword(e.target.value);
+                  }}
+                  style={{ marginBottom: "0.5rem" }}
+                ></input>
+              </div>
+            </div>
+          ) : (
+            <div className={styles.loginForm}></div>
+          )}
+        </div>
+      </div>
+    </main>
+  );
 }

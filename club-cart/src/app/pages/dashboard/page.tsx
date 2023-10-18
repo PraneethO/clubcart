bb28cab1810c7a1ab0e8fe7afcad43f1868efa16
cardinal73 <109763528+cardinal73@users.noreply.github.com>
2023-10-18 07:22:29 -0400

    started dash

--- club-cart/src/app/pages/dashboard/page.tsx
+++ club-cart/src/app/pages/dashboard/page.tsx
@@ -1,7 +1,45 @@
+import Link from "next/link";
 import styles from "./page.module.css";
+import FilterDropdown from "./filterDropdown";
 
 export default function Dashboard() {
     return <main className={styles.main}>
-        idfk
+        <div className={styles.nav}>
+            <Link href="/pages/dashboard" style={{ textDecoration: "none", color: "#044e8b" }}>
+                <div className={styles.navLogo}>CLUBCART</div>
+            </Link>
+            <Link href="/pages/cart" className={styles.link} style={{ marginLeft: "auto" }}>
+                <button className={styles.navButton}>
+                    <img src="/shopping-cart.png" width={35} height={35} style={{ marginLeft: "auto", marginRight: "auto", transform: "translateX(-2px)"}} />
+                    cart
+                </button>
+            </Link>
+            <Link href="/pages/profile" className={styles.link} style={{ marginLeft: "1rem" }}>
+                <button className={styles.navButton}>
+                    <img src="/default-avatar.png" width={35} height={35} style={{ marginLeft: "auto", marginRight: "auto"}} />
+                    profile
+                </button>
+            </Link>
+        </div>
+        <div className={styles.dashContainer}>
+
+            <div className={styles.dashLabelContainer}>
+                <div className={styles.filterLabel}>
+                    Filter
+                </div>
+                <div className={styles.dashLabel}>
+                    Shop
+                </div>
+            </div>
+
+            <div className={styles.dashContentContainer}>
+                <div className={styles.filter}>
+                    <FilterDropdown/>
+                </div>
+                <div className={styles.dashContent}>
+                    GG
+                </div>
+            </div>
+        </div>
     </main>;
 }

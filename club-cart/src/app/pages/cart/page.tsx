ef983a4efd308a7a97d7586164d3dca08b985c54
cardinal73 <109763528+cardinal73@users.noreply.github.com>
2023-10-19 14:09:09 -0400

    added cool underlines

--- club-cart/src/app/pages/cart/page.tsx
+++ club-cart/src/app/pages/cart/page.tsx
@@ -1,1 +1,39 @@
-""
+"use client"
+
+import React from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
+
+export default function Cart() {
+    return <main className={styles.main}>
+        <div className={styles.nav}>
+            <Link href="/pages/dashboard" style={{ textDecoration: "none", color: "#044e8b" }}>
+                <div className={styles.navLogo}>CLUBCART</div>
+            </Link>
+            <Link href="/pages/cart" className={styles.link} style={{ marginLeft: "auto" }}>
+                <button className={styles.navButton}>
+                    <img src="/shopping-cart.png" width={35} height={35} style={{ marginLeft: "auto", marginRight: "auto", transform: "translateX(-2px)" }} />
+                    cart
+                </button>
+            </Link>
+            <Link href="/pages/profile" className={styles.link} style={{ marginLeft: "1rem" }}>
+                <button className={styles.navButton}>
+                    <img src="/default-avatar.png" width={35} height={35} style={{ marginLeft: "auto", marginRight: "auto" }} />
+                    profile
+                </button>
+            </Link>
+        </div>
+        
+        <div className={styles.cartContainer}>
+
+            <div className={styles.cartLabelContainer}>
+                <div className={styles.itemsLabel}>
+                    Items
+                </div>
+                <div className={styles.orderTotalLabel}>
+                    Order Total: 
+                </div>
+            </div>
+        </div>
+    </main>;
+}

bd55672bf9bab88e0855c2f02bb85a055c0ceaad
cardinal73 <109763528+cardinal73@users.noreply.github.com>
2023-10-19 07:33:03 -0400

    started clubview

--- club-cart/src/app/pages/clubView/page.tsx
+++ club-cart/src/app/pages/clubView/page.tsx
@@ -2,9 +2,46 @@
 
 import React from "react";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Dashboard() {
     return <main className={styles.main}>
+        <div className={styles.nav}>
+            <Link href="/pages/dashboard" style={{ textDecoration: "none", color: "#044e8b" }}>
+                <div className={styles.navLogo}>CLUBCART</div>
+            </Link>
+            <Link href="/pages/cart" className={styles.link} style={{ marginLeft: "auto" }}>
+                <button className={styles.navButton}>
+                    <img src="/shopping-cart.png" width={35} height={35} style={{ marginLeft: "auto", marginRight: "auto", transform: "translateX(-2px)" }} />
+                    cart
+                </button>
+            </Link>
+            <Link href="/pages/profile" className={styles.link} style={{ marginLeft: "1rem" }}>
+                <button className={styles.navButton}>
+                    <img src="/default-avatar.png" width={35} height={35} style={{ marginLeft: "auto", marginRight: "auto" }} />
+                    profile
+                </button>
+            </Link>
+        </div>
 
+        <div className={styles.contentContainer}>
+            <div className={styles.imageContainer}>
+                hi
+            </div>
+            <div className={styles.clubContentContainer}>
+                <div className={styles.clubName}>
+                    PlaceHolder
+                </div>
+                <div className={styles.cost}>
+                    $35.00
+                </div>
+                <div className={styles.clubDescription}>
+                    Description
+                </div>
+                <button className={styles.clubExtra}>
+                    <div className={styles.addToCartText}>Add to Cart</div>
+                </button>
+            </div>
+        </div>
     </main>
 }

fb6c5dcc084f031d34f2ba5628bc63db05c0f094
cardinal73 <109763528+cardinal73@users.noreply.github.com>
2023-10-21 14:31:37 -0400

    did shit

--- club-cart/src/app/pages/payment/page.tsx
+++ club-cart/src/app/pages/payment/page.tsx
@@ -24,21 +24,78 @@ export default function Payment() {
 
         <div className={styles.paymentContainer}>
             <div className={styles.paymentLabelsContainer}>
-                <div className={styles.headerLabels}>
-                    Items
-                </div>
-
-                <div className={styles.orderTotalLabel}>
-                    <div className={styles.feeContainer}>
-                        <div className={styles.feeText}>
-                            Order Total
+                <div className={styles.paymentProcessContainer}>
+                    <div className={styles.headerLabels}>
+                        Contact Information
+                        <div className={styles.infoContainer}>
+                            <div className={styles.infoRow}>
+                                <div className={styles.descriptionContainer}>
+                                    <div className={styles.firstNameDescription}>First Name</div>
+                                    <div className={styles.lastNameDescription}>Last Name</div>
+                                </div>
+                                <div className={styles.nameInputContainer}>
+                                    <input className={styles.nameInput} placeholder="Enter Your First Name" />
+                                    <input className={styles.nameInput} placeholder="Enter Your Last Name" />
+                                </div>
+                            </div>
+                            <div className={styles.infoRow}>
+                                <div className={styles.descriptionContainer}>
+                                    <div className={styles.description}>Email</div>
+                                </div>
+                                <input className={styles.infoInput} placeholder="Enter Your Email"></input>
+                            </div>
+                            <button className={styles.continueButton}>Continue</button>
                         </div>
-                        <div className={styles.feePrice}>
-                            $90.50
+                    </div>
+
+                    <div className={styles.headerLabels}>
+                        Billing Address
+                        <div className={styles.infoRow}>
+                            <div className={styles.infoContainer}>
+                                <div className={styles.infoRow}>
+                                    <div className={styles.descriptionContainer}>
+                                        <div className={styles.description}>Address Line 1</div>
+                                    </div>
+                                    <input className={styles.infoInput} placeholder="Enter Your Street Address" />
+                                </div>
+
+                                <div className={styles.infoRow}>
+                                    <div className={styles.descriptionContainer}>
+                                        <div className={styles.description}>Address Line 2</div>
+                                    </div>
+                                    <input className={styles.infoInput} placeholder="Enter Your Apt, Suite, etc. (optional)" />
+                                </div>
+
+                                <div className={styles.infoRow}>
+                                    <div className={styles.descriptionContainer}>
+                                        <div className={styles.description}>ZIP Code Only (EX: 12345)</div>
+                                    </div>
+                                    <input className={styles.infoInput} placeholder="Enter Your ZIP Code" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={styles.headerLabels}>
+                        Payment
+                        <div className={styles.infoContainer}>
+
                         </div>
                     </div>
                 </div>
-                
+
+                <div className={styles.rightContainer}>
+                    <div className={styles.orderTotalLabel}>
+                        <div className={styles.feeContainer}>
+                            <div className={styles.feeText}>
+                                Order Total
+                            </div>
+                            <div className={styles.feePrice}>
+                                $90.50
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 

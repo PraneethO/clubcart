09bb55642602d44ca471004525b826dc4d07e9f0
cardinal73 <109763528+cardinal73@users.noreply.github.com>
2023-10-20 20:55:32 -0400

    cart done

--- club-cart/src/app/pages/cart/page.tsx
+++ club-cart/src/app/pages/cart/page.tsx
@@ -40,24 +40,76 @@ export default function Cart() {
                     <div className={styles.clubItem}>
                         <div className={styles.clubAttributes}>
                             <div className={styles.clubImage}>
-                                hi
+                                <img src="/default-avatar.png"/>
                             </div>
                             <div className={styles.attributes}>
                                 <div className={styles.attributesContainer}>
                                     <div className={styles.attributesClubName}>Club Name
                                     <div className={styles.attributesClubPrice}>$45.00</div></div>
-                                    
                                 </div>
                             </div>
                         </div>
+                        <div className={styles.itemBorder}></div>
                         <div className={styles.clubForms}>
-                            hi
+                            <div className={styles.clubFormsText}>
+                                - Registration Form
+                            </div>
+                            <div className={styles.clubFormsText}>
+                                - Health Form
+                            </div>
+                            <div className={styles.clubFormsText} style={{paddingBottom: "0"}}>
+                                - Registration Form
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className={styles.orderContent}>
-                    hi
-                </div>
+                <div className={styles.everythingOrderContainer}>
+                    <div className={styles.orderContent}>
+                        <div className={styles.feeContainer}>
+                            <div className={styles.feeText}>
+                                Speech and Debate
+                            </div>
+                            <div className={styles.feePrice}>
+                                $90.50
+                            </div>
+                        </div>
+
+                        <div className={styles.feeContainer}>
+                            <div className={styles.feeText}>
+                                Processing Fee
+                            </div>
+                            <div className={styles.feePrice}>
+                                $4.95
+                            </div>
+                        </div>
+                        <div className={styles.orderBorder}></div>
+                        
+                        <div className={styles.feeContainer}>
+                            <div className={styles.feeText}>
+                                Total
+                            </div>
+                            <div className={styles.feePrice}>
+                                $94.95
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.payNowButtonContainer}>
+                        <Link href="/pages/dashboard" style={{ textDecoration: "none"}}>
+                            <div className={styles.payNowButton}>
+                                Pay Now
+                            </div>
+                        </Link>
+                        <div className={styles.payNowText}>
+                            OR
+                        </div>
+                        {/* change to forms page */}
+                        <Link href="/pages/dashboard" style={{ textDecoration: "none"}}>
+                            <div className={styles.payLaterButton}>
+                                Continue To Forms
+                            </div>
+                        </Link>
+                    </div>
+                </div>    
             </div>
         </div>
     </main>;

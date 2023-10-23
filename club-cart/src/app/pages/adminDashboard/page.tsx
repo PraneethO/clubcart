907c1d6b2108af5b654d009365c73aef97041deb
cardinal73 <109763528+cardinal73@users.noreply.github.com>
2023-10-22 21:00:00 -0400

    joe

--- club-cart/src/app/pages/adminDashboard/page.tsx
+++ club-cart/src/app/pages/adminDashboard/page.tsx
@@ -37,23 +37,75 @@ export default function AdminDashboard() {
         <div className={styles.dashContainerContent}>
             <div className={styles.left}>
                 <div className={styles.headerLabels}>
-                    <button className={styles.dashboardButton}>Form Manager</button>
+                    <Link href="/pages/formManager">
+                        <button className={styles.dashboardButton}>Form Manager</button>
+                    </Link>
                     <div className={styles.infoContainer}>
 
                         <div className={styles.infoRow}>
                             <div className={styles.descriptionContainer}>
                                 <div className={styles.description}>Club Name</div>
                             </div>
-                            <input className={styles.infoInput} placeholder="Enter Your Email"></input>
+                            <input className={styles.infoInput} placeholder="Enter Your Club's Name"></input>
                         </div>
 
-                        <button className={styles.continueButton}>Continue to Forms Manager</button>
+                        <div className={styles.infoRow}>
+                            <div className={styles.descriptionContainer}>
+                                <div className={styles.firstHalfDescription}>DUES</div>
+                                <div className={styles.lastHalfDescription}>Number of Additional Forms</div>
+                            </div>
+                            <div className={styles.splitInputContainer}>
+                                <input
+                                    className={styles.splitInput}
+                                    placeholder="Enter The Amount of Dues (enter numbers only)"
+                                />
+                                <input
+                                    className={styles.splitInput}
+                                    placeholder="Enter the Number of External Forms You Would Like Include"
+                                    type="number"
+                                />
+                            </div>
+                        </div>
+
+                        {/* <div className={styles.infoRow}>
+                            <div className={styles.descriptionContainer}>
+                                <div className={styles.description}>Additional Forms</div>
+                            </div>
+                            <input className={styles.infoInput} placeholder="Paste The Link to Any External Forms"></input>
+                        </div> */}
+
+                        <Link href="/pages/formManager" style={{ textDecoration: "none" }}>
+                            <button className={styles.continueButton}>Continue to Form Manager</button>
+                        </Link>
+                    </div>
+                </div>
+
+                <div className={styles.headerLabels}>
+                    <Link href="/pages/formManager">
+                        <button className={styles.dashboardButton}>CHANGE</button>
+                    </Link>
+                    <div className={styles.infoContainer}>
+
+                        <div className={styles.infoRow}>
+                            <div className={styles.descriptionContainer}>
+                                <div className={styles.description}>Club Name</div>
+                            </div>
+                            <input className={styles.infoInput} placeholder="Enter Your Club's Name"></input>
+                        </div>
+
+                        <Link href="/pages/formManager" style={{ textDecoration: "none" }}>
+                            <button className={styles.continueButton}>Continue to (CHANGE)</button>
+                        </Link>
                     </div>
                 </div>
             </div>
 
             <div className={styles.right}>
-                <div className={styles.actionItemsContainer}>i</div>
+                <div className={styles.actionItemsContainer}>
+                    <div className={styles.actionItem}>- Forms</div>
+                    <div className={styles.actionItem}>- Forms</div>
+                    <div className={styles.actionItem}>- Forms</div>
+                </div>
             </div>
 
         </div>
---
+++ club-cart/src/app/pages/formManager/page.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
+
+export default function AdminDashboard() {
+    return <main className={styles.main}>
+        <div className={styles.nav}>
+            <Link href="/pages/adminDashboard" style={{ textDecoration: "none", color: "#044e8b" }}>
+                <div className={styles.navLogo}>CLUBCART</div>
+            </Link>
+            <Link href="/pages/adminDashboard" className={styles.link} style={{ marginLeft: "auto" }}>
+                <button className={styles.navButton}>
+                    <img src="/forms-icon.png" width={40} height={40} style={{ marginLeft: "auto", marginRight: "auto", transform: "translateX(-2px)" }} />
+                    forms
+                </button>
+            </Link>
+            <Link href="/pages/adminDashboard" className={styles.link} style={{ marginLeft: "1rem" }}>
+                <button className={styles.navButton}>
+                    <img src="/default-avatar.png" width={40} height={40} style={{ marginLeft: "auto", marginRight: "auto" }} />
+                    profile
+                </button>
+            </Link>
+        </div>
+    </main>
+}
